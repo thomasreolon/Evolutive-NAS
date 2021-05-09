85757f1a2bd4901b04de21fdcfde65b9c483f699
thomasreolon <thomas.reolon@studenti.unitn.it>
2021-05-09 13:34:46 +0200

    fix bugs in plot scores

--- src/plot_scores.py
+++ src/plot_scores.py
@@ -12,20 +12,25 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 
 import matplotlib.pyplot as plt
+import sys, gc
 
-from fne.genotopheno import VisionNetwork
+from fne.genotopheno import LearnableCell, VisionNetwork
 from fne.evolution import Mutations
 from fne.evolution.fitness import score_NTK, score_linear, n_params
+from fne.evolution.utils import print_memory, print_, clear_cache
 
-print('loading dataset: ', end='')
+# dataset
+
+print_('loading dataset:         ')
 
 transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    [transforms.Resize(224),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 dataset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform)
-
-print('done')
+loader = torch.utils.data.DataLoader(dataset, 8,  True)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # random networks
 
@@ -38,35 +43,47 @@ def get_random_net(n_classes):
                     'skip_connect', 'clinc_3x3', 'clinc_7x7', 'avg_pool_3x3',  'max_pool_3x3'}
     genotype = '0|0|2|0|0|2|0|0  1|0|0|1|1|0|0|0  0|1|0|0|0|0|2|1--1  7'
     mutator = Mutations(search_space, prob_mutation=0.8,
-                        prob_resize=0.99, prob_swap=0.99)
-    for i in range(10):
+                        prob_resize=0.4, prob_swap=0.6)
+    for i in range(6):
         genotype = mutator(genotype)
-    return VisionNetwork(3, n_classes, [genotype], search_space, 1)
+    """
+    net = nn.Sequential(
+        LearnableCell(3, genotype, search_space),
+        nn.AdaptiveAvgPool2d(1),
+        nn.Linear(c_out, n_classes)
+    )
+    """
+    net = VisionNetwork(3, n_classes, [genotype], search_space, 1)
+    return net
 
 # famous networks
 
 
-print('defining architectures: ', end='')
+print_('defining architectures:  ')
 
 
 def get_alexnet(n_classes):
     alexnet = models.alexnet()
     alexnet.classifier[6] = nn.Linear(4096, n_classes)
+    return alexnet
 
 
 def get_resnet(n_classes):
     resnet = models.resnet18()
-    resnet.classifier[6] = nn.Linear(512, n_classes)
+    resnet.fc = nn.Linear(512, n_classes)
+    return resnet
 
 
 def get_vgg(n_classes):
     model = models.vgg16()
     model.classifier[6] = nn.Linear(4096, n_classes)
+    return model
 
 
 def get_densenet(n_classes):
     model = models.densenet161()
-    model.classifier[6] = nn.Linear(4096, n_classes)
+    model.classifier = nn.Linear(4096, n_classes)
+    return model
 
 
 # hancrafted networks
@@ -83,52 +100,63 @@ def net3(n_classes):
     return nn.Sequential(nn.Conv2d(3, 32, 11), nn.ReLU(inplace=True), nn.AdaptiveMaxPool2d(5), nn.Dropout(.3), nn.Linear(32*5**2, 128), nn.ReLU(inplace=True), nn.Linear(128, n_classes), nn.Softmax())
 
 
-print('done')
+print('Done')
 
 # get score models
-
-loader = torch.utils.data.DataLoader(dataset, 64,  True)
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
 
 def get_scores(model):
     s1 = score_NTK(loader, model, device, 200)
     s2 = score_linear(loader, model, device, 200)
-    return s1, s2
+    s3 = s1*s2*n_params(model)
+    return s1, s2, s3
 
 
 # test models
+
+print_('getting scores:          0% ')
 n_classes = 10
 
-print('getting scores: 0% ', end='')
 rand_scores = []
-for _ in range(30):
+for _ in range(1):
+    clear_cache()
     model = get_random_net(n_classes)
+    model = model.to(device)
     rand_scores.append(get_scores(model))
 
-print('60% ', end='')
+exit(0)
+
+print_('60% ')
 famous_scores = []
 for getter in [get_alexnet, get_resnet, get_vgg, get_densenet]:
+    clear_cache()
     model = getter(n_classes)
+    model = model.to(device)
     famous_scores.append(get_scores(model))
 
-print('90% ', end='')
+print_('90% ')
 hand_scores = []
 for getter in [net1, net2, net3]:
+    clear_cache()
     model = getter(n_classes)
+    model = model.to(device)
     hand_scores.append(get_scores(model))
 
 print('100%')
-# plot
-
 scores = [rand_scores, famous_scores, hand_scores]
+with open('results.txt', 'w') as fout:
+    import json
+    json.dump(scores, fout)
+
+# plot first 2 scores
+
 colors = ['r', 'g', 'b']
 labels = ['random architecture',
-          'popolar architecture', 'average architecture']
+          'popular architecture', 'average architecture']
+print(scores)
 
 for s, c, l in zip(scores, colors, labels):
-    x = [v[0] for v in scores]
-    y = [v[1] for v in scores]
+    x = [v[0] for v in s]
+    y = [v[1] for v in s]
     plt.scatter(x, y, color=c, label=l)
 
 plt.title('scores for different architectures: better closer to origin')
@@ -136,3 +164,17 @@ plt.xlabel('Neural Tangent Kernel Score')
 plt.ylabel('Linear Region Score')
 plt.legend()
 plt.show()
+
+# plot 3° feat
+count = 0
+for s, c, l in zip(scores, colors, labels):
+    x = [count+i for i in range(len(s))]
+    y = [v[2] for v in s]
+    plt.scatter(x, y, color=c, label=l)
+    count += len(scores)
+
+plt.title('relation between architectures and the third score')
+plt.ylabel('score1*score2*n_params')
+plt.legend()
+plt.show()
+
